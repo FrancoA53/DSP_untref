78b9e95d505fe93c5154fdefd716a693da9016d7
FrancoA53 <francoareco53@gmail.com>
2021-07-01 21:14:02 -0300

    Funciones a presentar

---
+++ Griffin_Lim/Griffin_Lim.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  1 20:55:30 2021
+
+@author: ACER
+"""
+
+
+import numpy as np
+import librosa as lb
+from scipy import signal as sgn
+
+
+def griffin_lim(x,fs,iteracion,ventana = 'hamming'):
+    
+    f1,t1,y = sgn.stft(x,fs, window = ventana ,nperseg = 1000)
+    y_real = np.real(y)
+    matriz_compleja = np.random.randn(*y_real.shape)
+    matriz_reconstruida = y_real + 1j*matriz_compleja
+    b, ISTFT_inicial = sgn.istft(matriz_reconstruida,fs,window = ventana , nperseg = 1000)
+    
+    counter= 0
+    
+    while counter<iteracion:
+    
+        f2,t2,inicio = sgn.stft(ISTFT_inicial,fs, window = ventana , nperseg = 1000)
+        inicio_compleja = np.imag(inicio)
+        E = y_real * np.exp(1j*inicio_compleja)
+        t,ISTFT_initial = sgn.istft(E,fs,window = ventana , nperseg = 1000)
+        counter += 1
+    
+    return t,ISTFT_initial
+
+def griffin_lim2(x,fs,iteracion,ventana = 'hamming'):
+    
+    x_mag = lb.stft(x,fs,window = ventana, nperseg = 1000)
+     
+    M = abs(x_mag)
+    
+    P = np.random.randn(*M.shape)
+    
+    for i in range(iteracion):
+                
+        D = M * np.exp(1j * P)
+        aproximacion = lb.istft(D)    
+        
+    return aproximacion
